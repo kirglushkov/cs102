340c1365dfbc4a68aa5f10a49512ed19d2567f3e
kirglushkov <kirglushkovv@gmail.com>
2021-10-11 14:43:36 +0300

    I did caesar.py!

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,46 +1,26 @@
-import typing as tp
-
-
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
+    import string
+    alphabet_lower = string.ascii_lowercase
+    alphabet_upper = string.ascii_uppercase
+    shifted_alphabet_lower = alphabet_lower[shift:] + alphabet_lower[:shift]
+    shifted_alphabet_upper = alphabet_upper[shift:] + alphabet_upper[:shift]
 
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
+    alphabet = alphabet_lower + alphabet_upper
+    shifted_alphabet = shifted_alphabet_lower + shifted_alphabet_upper
 
+    table = str.maketrans(alphabet, shifted_alphabet)
+    return plaintext.translate(table)
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
+    import string
+    shift = 26 - shift
+    alphabet_lower = string.ascii_lowercase
+    alphabet_upper = string.ascii_uppercase
+    shifted_alphabet_lower = alphabet_lower[shift:] + alphabet_lower[:shift]
+    shifted_alphabet_upper = alphabet_upper[shift:] + alphabet_upper[:shift]
 
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
-    plaintext = ""
-    # PUT YOUR CODE HERE
-    return plaintext
+    alphabet = alphabet_lower + alphabet_upper
+    shifted_alphabet = shifted_alphabet_lower + shifted_alphabet_upper
 
-
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
-    best_shift = 0
-    # PUT YOUR CODE HERE
-    return best_shift
+    table = str.maketrans(alphabet, shifted_alphabet)
+    return ciphertext.translate(table)
