a0db9aa05ae7ebdad8ede0c190f773404690b23c
kirglushkov <kirglushkovv@gmail.com>
2021-10-11 15:04:08 +0300

    Create some changes

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,4 +1,20 @@
+import typing as tp
+
+
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
+    """
+    Encrypts plaintext using a Caesar cipher.
+
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+    ciphertext = ""
     import string
     alphabet_lower = string.ascii_lowercase
     alphabet_upper = string.ascii_uppercase
@@ -9,9 +25,23 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     shifted_alphabet = shifted_alphabet_lower + shifted_alphabet_upper
 
     table = str.maketrans(alphabet, shifted_alphabet)
-    return plaintext.translate(table)
+    ciphertext = plaintext.translate(table)
+    return ciphertext
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
+    """
+    Decrypts a ciphertext using a Caesar cipher.
+
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
+    plaintext = ""
     import string
     shift = 26 - shift
     alphabet_lower = string.ascii_lowercase
@@ -21,6 +51,6 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
 
     alphabet = alphabet_lower + alphabet_upper
     shifted_alphabet = shifted_alphabet_lower + shifted_alphabet_upper
-
     table = str.maketrans(alphabet, shifted_alphabet)
-    return ciphertext.translate(table)
+    plaintext = ciphertext.translate(table)
+    return plaintext
