3ae10c138bda989278f7dd84992620ea3956a8e7
kirglushkov <kirglushkovv@gmail.com>
2021-10-12 14:32:36 +0300

    i did rsa black

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -20,7 +20,7 @@ def is_prime(n: int) -> bool:
     sqr = int(math.sqrt(n)) + 1
 
     for divisor in range(3, sqr, 2):
-        if n % divisor ==0:
+        if n % divisor == 0:
             return False
     return True
     pass
@@ -35,18 +35,18 @@ def gcd(a: int, b: int) -> int:
     1
     """
     # Everything divides 0
-    if (a == 0):
+    if a == 0:
         return b
 
-    if (b == 0):
+    if b == 0:
         return a
 
     # base case
-    if (a == b):
+    if a == b:
         return a
 
     # a is greater
-    if (a > b):
+    if a > b:
         return gcd(a - b, b)
     return gcd(a, b - a)
     pass
@@ -61,7 +61,7 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     """
     for i in range(0, phi):
         # If we have our multiplicative inverse then return it
-        if (e*i) % phi == 1:
+        if (e * i) % phi == 1:
             return i
     # If we didn't find the multiplicative inverse in the loop above
     # then it doesn't exist for A under M
@@ -69,7 +69,9 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     pass
 
 
-def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+def generate_keypair(
+    p: int, q: int
+) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
     elif p == q:
@@ -77,7 +79,7 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
 
     # n = pq
     n = p * q
-    phi = (p-1)*(q-1)
+    phi = (p - 1) * (q - 1)
     # PUT YOUR CODE HERE
 
     # Choose an integer e such that e and phi(n) are coprime
