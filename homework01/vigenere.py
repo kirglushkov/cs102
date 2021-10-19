2c782ecee71a3c62416fbad47d52d5fb1f0e16f0
kirglushkov <kirglushkovv@gmail.com>
2021-10-19 20:59:57 +0300

    Rewrite vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -8,38 +8,27 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    ciphertext = ""
-    if plaintext.islower():
-        import string
+    ciphertext = []
+    import string
 
-        alphabet1 = string.ascii_lowercase
-        l_to_index = dict(zip(alphabet1, range(len(alphabet1))))
-        index_for_l = dict(zip(range(len(alphabet1)), alphabet1))
-        split_word = [
-            plaintext[i : i + len(keyword)] for i in range(0, len(plaintext), len(keyword))
-        ]
-        for each in split_word:
-            i = 0
-            for letter in each:
-                number = (l_to_index[letter] + l_to_index[keyword[i]]) % len(alphabet1)
-                ciphertext = ciphertext + index_for_l[number]
-                i = i + 1
-    else:
-        import string
-
-        alphabet2 = string.ascii_uppercase
-        l_to_index = dict(zip(alphabet2, range(len(alphabet2))))
-        index_for_l = dict(zip(range(len(alphabet2)), alphabet2))
-        split_word = [
-            plaintext[i : i + len(keyword)] for i in range(0, len(plaintext), len(keyword))
-        ]
-        for each in split_word:
-            i = 0
-            for letter in each:
-                number = (l_to_index[letter] + l_to_index[keyword[i]]) % len(alphabet2)
-                ciphertext = ciphertext + index_for_l[number]
-                i = i + 1
-    return ciphertext
+    alphabet1 = string.ascii_uppercase
+    alphabet2 = string.ascii_lowercase
+    i = 0
+    for each in plaintext:
+        if each in alphabet1:
+            offset = ord(keyword[i]) - ord("A")
+            encrypted = chr((ord(each) - ord("A") + offset) % 26 + ord("A"))
+            ciphertext += encrypted
+            i = (i + 1) % len(keyword)
+        elif each in alphabet2:
+            offset = ord(keyword[i]) - ord("a")
+            encrypted = chr((ord(each) - ord("a") + offset) % 26 + ord("a"))
+            ciphertext += encrypted
+            i = (i + 1) % len(keyword)
+        else:
+            ciphertext += each
+            i = (i + 1) % len(keyword)
+    return "".join([str(i) for i in ciphertext])
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
@@ -52,36 +41,26 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    plaintext = ""
-    if ciphertext.islower():
-        import string
+    plaintext = []
+    import string
 
-        alphabet1 = string.ascii_lowercase
-        l_to_index = dict(zip(alphabet1, range(len(alphabet1))))
-        index_for_l = dict(zip(range(len(alphabet1)), alphabet1))
-        split_ciphertext = [
-            ciphertext[i : i + len(keyword)] for i in range(0, len(ciphertext), len(keyword))
-        ]
-        for each in split_ciphertext:
-            i = 0
-            for letter in each:
-                number = (l_to_index[letter] - l_to_index[keyword[i]]) % len(alphabet1)
-                plaintext = plaintext + index_for_l[number]
-                i = i + 1
-    else:
-        import string
-
-        alphabet2 = string.ascii_uppercase
-        l_to_index = dict(zip(alphabet2, range(len(alphabet2))))
-        index_for_l = dict(zip(range(len(alphabet2)), alphabet2))
-        split_ciphertext = [
-            ciphertext[i : i + len(keyword)] for i in range(0, len(ciphertext), len(keyword))
-        ]
-        for each in split_ciphertext:
-            i = 0
-            for letter in each:
-                number = (l_to_index[letter] - l_to_index[keyword[i]]) % len(alphabet2)
-                plaintext = plaintext + index_for_l[number]
-                i = i + 1
-
-    return plaintext
+    alphabet1 = string.ascii_uppercase
+    alphabet2 = string.ascii_lowercase
+    j = 0
+    for each in ciphertext:
+        if each in alphabet1:
+            offset = ord(keyword[j]) - ord("A")
+            decrypted_offset = 26 - offset
+            decrypted = chr((ord(each) - ord("A") + decrypted_offset) % 26 + ord("A"))
+            plaintext += decrypted
+            j = (j + 1) % len(keyword)
+        elif each in alphabet2:
+            offset = ord(keyword[j]) - ord("a")
+            decrypted_offset = 26 - offset
+            decrypted = chr((ord(each) - ord("a") + decrypted_offset) % 26 + ord("a"))
+            plaintext += decrypted
+            j = (j + 1) % len(keyword)
+        else:
+            plaintext += each
+            j = (j + 1) % len(keyword)
+    return "".join([str(i) for i in plaintext])
