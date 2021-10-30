c197e1aa8d66b1b0544edb2c7303df3f5a818013
kirglushkov <kirglushkovv@gmail.com>
2021-10-30 20:33:03 +0300

    rsa changes

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -34,20 +34,9 @@ def gcd(a: int, b: int) -> int:
     1
     """
     # Everything divides 0
-    if a == 0:
-        return b
-
-    if b == 0:
-        return a
-
-    # base case
-    if a == b:
-        return a
-
-    # a is greater
-    if a > b:
-        return gcd(a - b, b)
-    return gcd(a, b - a)
+    while b:
+        a, b = b, a % b
+    return a
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
