4cf128c71a5eb7bc66e3c3701d4b418239fba278
Kirill Glushkov <kirglushkovv@gmail.com>
2022-04-10 12:19:02 +0300

    unittest3

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,5 +2,5 @@ VK_CONFIG = {
     "domain": "https://api.vk.com/method/",
     "access_token": "778f2563132667012b4b91b1ea004a1b8baf0d6113cf292c4575c1339b0ddaae595dff46d46b95aaae9a4",
     "client_id": "8090577",
-    "version": "5.126",
+    "version": "5.131",
 }
