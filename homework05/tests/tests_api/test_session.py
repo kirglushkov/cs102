5e90382f1a34332417f5c72bc3da294d22e07cce
Kirill Glushkov <kirglushkovv@gmail.com>
2022-04-10 11:45:14 +0300

    myoy

--- homework05/tests/tests_api/test_session.py
+++ homework05/tests/tests_api/test_session.py
@@ -1,10 +1,10 @@
 import time
 import unittest
 
-import httpretty
-import responses
-from requests.exceptions import ConnectionError, HTTPError, ReadTimeout, RetryError
-from vkapi.session import Session
+import httpretty  # type: ignore
+import responses  # type: ignore
+from requests.exceptions import ConnectionError, HTTPError, ReadTimeout, RetryError  # type: ignore
+from vkapi.session import Session  # type: ignore
 
 
 class TestSession(unittest.TestCase):
