791009c4546b4f13027d01d011ee9822f1306d02
Kirill Glushkov <90201085+kirglushkov@users.noreply.github.com>
2022-04-10 16:18:08 +0300

    Update test_session.py

--- homework05/tests/tests_api/test_session.py
+++ homework05/tests/tests_api/test_session.py
@@ -3,7 +3,7 @@ import unittest
 
 import httpretty  # type: ignore
 import responses  # type: ignore
-from requests.exceptions import ConnectionError, HTTPError, ReadTimeout, RetryError  # type: ignore
+from requests.exceptions import ConnectionError, HTTPError, ReadTimeout, RetryError
 from vkapi.session import Session  # type: ignore
 
 
